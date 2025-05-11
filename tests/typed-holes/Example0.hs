633f67669170525ad9d111fe0415c2ab32413ad7
Matheus de Sousa Bernardo <matheussbernardo@gmail.com>
2025-05-11 15:39:37 +0200

    Add new version detect synthesis

--- tests/typed-holes/Example0.hs
+++ tests/typed-holes/Example0.hs
@@ -1,18 +1,10 @@
-{-@ LIQUID "--expect-error-containing=AAAAA" @-}
+{-@ LIQUID "--expect-error-containing=Hole Found" @-}
 {-@ LIQUID "--warn-on-term-holes" @-}
 
 module Example0 where
     hole = undefined
 
-    {-@ foo :: {v : Int | v == 1} @-}
-    foo :: Int
-    foo = hole + 1
-
-    {-@ bar :: {v : Int | v == 1} @-}
-    bar :: Int
-    bar = hole - 1
-
-    -- {-@ listLength :: xs:[a] -> {v : Nat | v == len xs} @-}
-    -- listLength :: [a] -> Int
-    -- listLength [] = hole
-    -- listLength (_:xs) = 1 + listLength xs
+    {-@ listLength :: xs:[a] -> {v : Nat | v == len xs} @-}
+    listLength :: [a] -> Int
+    listLength [] = 0
+    listLength (_:xs) = 1 + hole
